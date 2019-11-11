5240ce6a7f01ebb91acf49ffbad100fb212088bb
Thinh Dang <tiffythinhdang@gmail.com>
2019-11-11 14:56:32 -0800

    Set up front end folder with dummy store and root reducer

---
+++ frontend/store/store.jsx
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import RootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const configureStore = (preloadedState = {}) => {
+  let middleware = [thunk];
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+  return createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(...middleware)
+  );
+};
+
+export default configureStore;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/bento_up.jsx',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, "app", "assets", "javascripts")
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      },
+      {
+        // The important stuff
+        test: /\.(jpg|jpeg|png)(\?.*)?$/, // Load only .jpg .jpeg, and .png files
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name][md5:hash].[ext]', // Name of bundled asset
+            outputPath: 'webpack-assets/', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
+            publicPath: '/assets/webpack-assets/' // Endpoint asset can be found at on Rails server
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+    alias: {
+      assets: path.resolve('./app/assets')
+    }
+  }
+};
