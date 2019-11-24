501c28d13aa3ed3452a29aca12615416ec9441c1
Thinh Dang <tiffythinhdang@gmail.com>
2019-11-24 11:16:10 -0800

    Remove logger and thunk

--- frontend/store/store.jsx
+++ frontend/store/store.jsx
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
 
 const store = createStore(
   RootReducer,
   {},
-  applyMiddleware(thunk, logger)
+  // applyMiddleware(thunk, logger)
 );
 
 export default store;
